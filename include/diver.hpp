58edadd6874402efd81d3d32e928234d81954028
Vladislav Babkin <vibi.vlad24@gmail.com>
2026-05-26 16:42:47 +0300

    feat(diver): add movement with inertia, acceleration and placeholder graphics

--- include/diver.hpp
+++ include/diver.hpp
@@ -2,6 +2,7 @@
 #define DIVER_HPP
 
 #include <SFML/Graphics.hpp>
+#include <memory>
 
 namespace Game {
 
@@ -14,8 +15,19 @@ class Diver {
     void draw(sf::RenderWindow &window) const;
     sf::Vector2f getPosition() const;
 
+    void setTexture(const sf::Texture &texture);
+
   private:
     sf::Vector2f m_position;
+    sf::Vector2f m_velocity;
+    sf::Vector2f m_movingDirection;
+
+    float m_maxSpeed = 200.0f;
+    float m_acceleration = 500.0f;
+    float m_waterFading = 0.87f;
+    // Графическая часть
+    sf::Texture m_placeholderTexture;
+    std::unique_ptr<sf::Sprite> m_sprite;
 };
 } // namespace Game
 #endif
